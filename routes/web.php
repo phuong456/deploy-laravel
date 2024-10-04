ee2473625ede5e5348e9a7cd7a27e2039c0f3607
phuong456 <54199213+phuong456321@users.noreply.github.com>
2024-10-04 09:45:41 +0700

    commit main

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\GoogleController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallBack'])->name('auth.google.callback');
+
